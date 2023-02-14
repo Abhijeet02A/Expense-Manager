7e3b241cee1fca162185dd0ab96d2c969d05265a
Abhijit <abhijeetawari1402@gmail.com>
2023-02-14 15:40:50 +0530

    create separate component date

--- src/Components/ExpenseItems.js
+++ src/Components/ExpenseItems.js
@@ -1,12 +1,9 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItems.css'
-function ExpenseItem(props) {
-    const expenseDate = new Date(2023, 2, 13);
-    const expenseItem = "Car Insurance";
-    const expensePrice = 123.45;
-
+function ExpenseItem(props) {    
     return (
         <div className='expense-item'>
-            <div>{props.date.toISOString()}</div>
+            <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
